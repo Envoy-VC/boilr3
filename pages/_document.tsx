3a511c04b6e256cdcd808dddfa39ee3d39e51f88
Vedant Chainani <vedantchainani1084@gmail.com>
2023-04-23 03:17:02 +0530

    Add SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,88 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head>
+				{/* Character Set */}
+				<meta charSet='utf-8' />
+
+				{/* Description */}
+				<meta
+					name='description'
+					content='Accelerate your dApp development using boilr3, a feature-rich Next.js boilerplate with RainbowKit, Tailwind CSS, and WAGMI React Hooks.'
+				/>
+
+				{/* Favicon */}
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/favicon/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/favicon/site.webmanifest' />
+
+				{/* Robots Search Indexing */}
+				<meta
+					name='robots'
+					content='follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large'
+				/>
+
+				{/* Twitter Card data */}
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:site' content='@Envoy_1084' />
+				<meta
+					name='twitter:title'
+					content='boilr3: The Ultimate Next.js dApp Boilerplate with RainbowKit, Tailwind CSS & WAGMI'
+				/>
+				<meta
+					name='twitter:description'
+					content='Introducing boilr3: a Next.js dApp boilerplate featuring RainbowKit wallet authentication, Tailwind CSS styling, and WAGMI React Hooks for Ethereum'
+				/>
+				<meta name='twitter:image:alt' content='boilr3' />
+				<meta name='twitter:creator' content='@Envoy_1084' />
+				<meta
+					name='twitter:image'
+					content='https://i.ibb.co/YcMcqK6/Twitter-Card.png'
+				/>
+
+				{/* Open Graph data */}
+				<meta property='og:locale' content='en_US' />
+				<meta
+					property='og:title'
+					content='boilr3: Build Scalable Decentralized Apps with Next.js, RainbowKit, Tailwind CSS & WAGMI'
+				/>
+				<meta property='og:url' content='http://boilr3.vercel.app' />
+				<meta property='og:image' content='https://i.ibb.co/GRs4n32/OG.png' />
+				<meta property='og:image:type' content='image/png' />
+				<meta property='og:image:width' content='1200' />
+				<meta property='og:image:height' content='630' />
+				<meta name='og:image:alt' content='boilr3' />
+				<meta property='og:type' content='website' />
+				<meta
+					property='og:description'
+					content='Kickstart your dApp development with boilr3, a powerful Next.js boilerplate packed with RainbowKit wallet authentication, Tailwind CSS styling, and WAGMI React Hooks for Ethereum. Experience a solid foundation for your next decentralized app project.'
+				/>
+				<meta
+					property='og:site_name'
+					content='boilr3: Next.js dApp Boilerplate'
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Head from 'next/head';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { Inter } from 'next/font/google';
 import { Toaster, toast } from 'react-hot-toast';
@@ -38,8 +39,11 @@ export default function Home() {
 		<main
 			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
 		>
+			<Head>
+				<title>boilr3: Next.js dApp Boilerplate</title>
+			</Head>
 			<Toaster position='bottom-left' reverseOrder={false} />
-			<div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+			<div className='z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex'>
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
 					Get started by editing&nbsp;
 					<code className='font-mono font-bold'>pages/index.tsx</code>
@@ -90,7 +94,7 @@ export default function Home() {
 
 			<div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left mt-24 lg:mt-0'>
 				<a
-					href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app'
+					href='https://nextjs.org/'
 					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
 					target='_blank'
 					rel='noopener noreferrer'
@@ -108,7 +112,7 @@ export default function Home() {
 				</a>
 
 				<a
-					href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app'
+					href='https://www.rainbowkit.com/'
 					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
 					target='_blank'
 					rel='noopener noreferrer'
@@ -126,7 +130,7 @@ export default function Home() {
 				</a>
 
 				<a
-					href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app'
+					href='https://tailwindcss.com/'
 					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
 					target='_blank'
 					rel='noopener noreferrer'
@@ -144,7 +148,7 @@ export default function Home() {
 				</a>
 
 				<a
-					href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app'
+					href='https://github.com/Envoy-VC/boilr3.git'
 					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
 					target='_blank'
 					rel='noopener noreferrer'
